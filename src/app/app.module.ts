18f6f6c52681701285cb60ee504ce687f0bb3e5e
ziadhany <ziadhany2016@gmail.com>
2023-12-25 18:34:06 +0200

    Edit this app and make sure the frontend work fine with this backend https://github.com/ziadhany/todo-java-spring
    Signed-off-by: ziadhany <ziadhany2016@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { TodolistComponent } from './todolist/todolist.component';
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { TodolistComponent } from './todolist/todolist.component';
   ],
   imports: [
     FormsModule,
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/todolist/todolist.component.ts
+++ src/app/todolist/todolist.component.ts
@@ -1,40 +1,63 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-todolist',
   templateUrl: './todolist.component.html',
-  styleUrls: ['./todolist.component.css']
+  styleUrls: ['./todolist.component.css'],
 })
 export class TodolistComponent implements OnInit {
-  taskArray = [{ taskName: 'Brush teeth', isCompleted: false }];
+  taskArray = [{ id: 1, task: 'Brush teeth', completed: false }];
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.http.get<any>('http://127.0.0.1:8080/api/v1/tasks/').subscribe(data => {
+      this.taskArray = data;
+    });
   }
 
   onSubmit(form: NgForm) {
-    console.log(form);
-
-    this.taskArray.push({
-      taskName: form.controls['task'].value,
-      isCompleted: false
-    })
-
+    this.http.post<any>('http://127.0.0.1:8080/api/v1/tasks/',
+      {
+        "id": 10000,
+        "task": form.controls['task'].value,
+        "completed": false,
+      }
+    ).subscribe(data => {
+      console.log(data.id)
+      this.taskArray.push({
+        id: data.id,
+        task: data.task,
+        completed: data.completed,
+      })
+    });
     form.reset();
   }
 
-  onDelete(index: number) {
-    console.log(index);
-
-    this.taskArray.splice(index, 1);
+  onDelete(id: number) {
+    this.taskArray = this.taskArray.filter(function(item) {
+      return item.id !== id
+    })
+    this.http.delete<any>('http://127.0.0.1:8080/api/v1/tasks/' + id,
+      { body: { "id":id } }
+    ).subscribe()
   }
 
-  onCheck(index: number) {
-    console.log(this.taskArray);
+  onCheck(id: number, task: string) {
+    let mod_index= this.taskArray.findIndex(function(item) {
+      return item.id === id
+    });
 
-    this.taskArray[index].isCompleted = !this.taskArray[index].isCompleted;
+    this.taskArray[mod_index].completed = !this.taskArray[mod_index].completed;
+    this.http.put<any>('http://127.0.0.1:8080/api/v1/tasks/' + id,
+      {
+          "id": id,
+          "task": task,
+          "completed": this.taskArray[mod_index].completed,
+        }
+    ).subscribe()
   }
 
 }
